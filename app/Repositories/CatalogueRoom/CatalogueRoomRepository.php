110159c240f0112027264a9c7f1fccd074401472
Nam Hoang <hoangnam30102004hy@gmail.com>
2024-11-27 22:37:11 +0700

    up

--- app/Repositories/CatalogueRoom/CatalogueRoomRepository.php
+++ app/Repositories/CatalogueRoom/CatalogueRoomRepository.php
@@ -93,7 +93,7 @@ class   CatalogueRoomRepository extends BaseRepository implements CatalogueRoomI
         return $categories->map(function ($category) use ($roomIds, $startDate, $endDate, $numberChild, $numberAdult) {
             $filteredRooms = $category->rooms()->whereNotIn('id', $roomIds)
                 ->where('rooms.status', RoomStatusEnum::SAN_SANG_SU_DUNG->value)->get();
-            $attributes = $category->attribute;
+            $attributes = $category->attributes;
             return [
                 'id' => $category->id,
                 'name' => $category->name,
