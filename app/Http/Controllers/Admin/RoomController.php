ce6b00ed38e09ea6e2cb21a1ea90c31e5c1f18fd
Nam Hoang <hoangnam30102004hy@gmail.com>
2024-12-17 00:29:31 +0700

    edit admin room

--- app/Http/Controllers/Admin/RoomController.php
+++ app/Http/Controllers/Admin/RoomController.php
@@ -47,20 +47,20 @@ class RoomController extends Controller
     public function store(RoomRequest $request)
     {
         $this->roomService->create($request);
-        return redirect()->route('admin.rooms.index')->with('success', 'Thêm mới thành công!');
+        return redirect()->back()->with('success', 'Thêm mới thành công!');
     }
 
 
     public function update(RoomRequest $request, $id)
     {
         $this->roomService->update($id, $request);
-        return redirect()->route('admin.rooms.index')->with('success', 'Cập nhật thành công!');
+        return redirect()->back()->with('success', 'Cập nhật thành công!');
     }
 
 
     public function destroy($id)
     {
         $this->roomService->delete($id);
-        return redirect()->route('admin.rooms.index')->with('success', 'Xóa thành công!');
+        return redirect()->back()->with('success', 'Xóa thành công!');
     }
 }
